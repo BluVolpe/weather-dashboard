005530e64f3ba7811b16195aab3caed209f096c4
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-04-15 18:46:36 -0400

    added html and js from start up

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const apiKey = "d9c0aaf84487d1a17ce6e646159d3882";
+const searchBtn = document.querySelector("#search-button")
+
+
+
+
+function searchValue() {
+    let searchValue = document.querySelector("#search-value").value;
+    // TODO set up validation if form is blank
+    console.log(searchValue);
+    getCurrentWeather(searchValue);
+}
+
+function getCurrentWeather(searchValue) {
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + apiKey;
+
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data);
+            // TODO append content to the DOM either right here or with a function
+            // TODO call five day forecast and UV index
+        })
+}
+
+
+
+searchBtn.addEventListener("click", searchValue);
