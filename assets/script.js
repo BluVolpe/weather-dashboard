ab4097f09a70da7eae5640fea235db4993ba9424
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-04-15 18:50:08 -0400

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+const apiKey = "d9c0aaf84487d1a17ce6e646159d3882";
+const searchBtn = document.querySelector("#search-button")
+
+
+
+
+function searchValue() {
+    let searchValue = document.querySelector("#search-value").value;
+    // TODO set up validation if form is blank
+    console.log(searchValue);
+    getCurrentWeather(searchValue);
+}
+
+function getCurrentWeather(searchValue) {
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + apiKey;
+
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data);
+            // TODO append content to the DOM either right here or with a function
+            // TODO call five day forecast and UV index
+        })
+}
+
+
+
+=======
 const apiKey = "d9c0aaf84487d1a17ce6e646159d3882";
 const searchBtn = document.querySelector("#search-button")
 
@@ -11,4 +41,5 @@ function searchValue() {
 
 
 
+>>>>>>> b88f6d7b7e0684aa4dd569e6a35d6284fac4999a
 searchBtn.addEventListener("click", searchValue);
